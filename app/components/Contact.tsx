39386f6747c1a8d4f4a27cdbe866270661b0bcb7
Nguyen Van Hung <nv-hung@linkstaff.co.jp>
2024-04-22 18:48:25 +0900

    second commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,4 +1,30 @@
+"use client"
+
+import { ChangeEvent, useState } from "react"
+
+type TForm = {
+    name: string,
+    phone: string,
+    message: string
+}
+
+const initialForm = {
+    name: "",
+    phone: "",
+    message: ""
+}
+
 const Contact = () => {
+    const [form, setForm] = useState<TForm>(initialForm);
+
+    const handleChangeData = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setForm(prevForm => ({
+            ...prevForm,
+            [name]: value
+        }));
+    }
+
     return (
         <div className="w-full pt-[120px]" id="contact">
             <div className="px-12 lg:px-40">
@@ -27,19 +53,19 @@ const Contact = () => {
                         </div>
                     </div>
                     <div className="flex-1 py-10">
-                        <div className="border-2 border-gray-400 w-[70%] mx-auto my-10 lg:hidden"/>
+                        <div className="border-2 border-gray-400 w-[70%] mx-auto my-10 lg:hidden" />
                         <div className="grid w-full lg:w-[80%] mx-auto gap-3">
                             <div className="w-full">
-                                <input type="text" className="border-2 border-gray-400 p-3 bg-transparent w-full rounded-md" placeholder="Name" />
+                                <input type="text" className="border-2 border-gray-400 p-3 bg-transparent w-full rounded-md" placeholder="Name" name="name" value={form.name} onChange={handleChangeData}/>
                             </div>
                             <div className="w-full">
-                                <input type="text" className="border-2 border-gray-400 p-3 bg-transparent w-full rounded-md" placeholder="Phone" />
+                                <input type="text" className="border-2 border-gray-400 p-3 bg-transparent w-full rounded-md" placeholder="Phone" name="phone" value={form.phone} onChange={handleChangeData}/>
                             </div>
                             <div className="w-full">
-                                <textarea className="h-[300px] border-2 border-gray-400 p-3 bg-transparent w-full rounded-md" placeholder="Message" />
+                                <textarea className="h-[300px] border-2 border-gray-400 p-3 bg-transparent w-full rounded-md" placeholder="Message" name="message" value={form.message} onChange={handleChangeData}/>
                             </div>
                             <div className="w-full">
-                                <button className="w-full py-2 px-5 bg-blue-400 rounded-md">Submit</button>
+                                <button className="w-full py-2 px-5 bg-blue-400 rounded-md" onClick={() => setForm(initialForm)}>Submit</button>
                             </div>
                         </div>
                     </div>
